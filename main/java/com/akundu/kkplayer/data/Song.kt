c4390cfff32b40a0261709554be79cde946596cc
gerasymenkoke <104980001+gerasymenkoke@users.noreply.github.com>
2023-05-13 08:31:16 +0300

    Delete main directory

--- main/java/com/akundu/kkplayer/data/Song.kt
+++
@@ -1,9 +0,0 @@
-package com.akundu.kkplayer.data
-
-data class Song(
-    val title: String,
-    val artist: String,
-    val fileName: String,
-    val url: String,
-    val movie: String
-)
--- main/java/com/akundu/kkplayer/provider/AppMediaProvider.kt
+++
@@ -1,6 +0,0 @@
-package com.akundu.kkplayer.provider
-
-import androidx.core.content.FileProvider
-import com.akundu.kkplayer.R
-
-class AppMediaProvider : FileProvider(R.xml.provider_paths)
